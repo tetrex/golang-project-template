5159326f34c1f2e355b8d8e150f7c4709d78b9f1
harsh rawat <42332761+tetrex@users.noreply.github.com>
2024-09-10 18:36:06 +0530

    fix: routes

---
+++ cmd/server/router.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+func setupRoutes(router *echo.Echo, services *Services, l *zerolog.Logger) {
+	router.GET("/", services.Health.HealthCheck)
+
+	router.GET("docs/*", echoSwagger.WrapHandler)
+
+	l.Info().Msgf("docs v1 at :: http://localhost:%d/docs/index.html \n", 8000)
+
+}
